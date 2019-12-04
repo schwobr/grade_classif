5dd6b288dfc93c8b7f41ad68da8d09c3a14a78cb
Robin Schwob <robin.schwob@student.ecp.fr>
2019-12-04 18:35:01 +0100

    debugging doc building

--- grade_classif/core.py
+++ grade_classif/core.py
@@ -3,11 +3,6 @@
 __all__ = ['ifnone']
 
 #Cell
-from test_tube import HyperOptArgumentParser
-from .defaults import *
-from pathlib import Path
-
-#Cell
 def ifnone(a, b):
     "`b` if `a` is None else `a`"
     return b if a is None else a
